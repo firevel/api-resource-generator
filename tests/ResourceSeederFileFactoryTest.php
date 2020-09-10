e799e6ae98934f24d80b236122b8e63dee88f870
Gustavo Gentil <gustavorgentil@outlook.com>
2020-09-10 19:54:21 -0300

    test: ensure ResourceSeederFileFactory creates the seeder correctly

---
+++ tests/ResourceSeederFileFactoryTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Firevel\ApiResourceGenerator\Tests;
+
+use Firevel\ApiResourceGenerator\DirectoryMakerFacade as DirectoryMaker;
+use Firevel\ApiResourceGenerator\Factories\ResourceSeederFileFactory;
+use Firevel\ApiResourceGenerator\FileMakerFacade as FileMaker;
+use Firevel\ApiResourceGenerator\Resource;
+use Firevel\ApiResourceGenerator\StubBuilderFacade as StubBuilder;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class ResourceSeederFileFactoryTest extends TestCase
+{
+    use WithFaker;
+
+    /** @test */
+    public function it_creates_the_policies_directory_if_it_does_not_exist(): void
+    {
+        $resource = new Resource($this->faker->word);
+
+        FileMaker::shouldReceive('make')->once();
+        StubBuilder::shouldReceive('build')->once();
+
+        DirectoryMaker::shouldReceive('findOrMake')
+            ->with(database_path('seeds'))
+            ->once();
+
+        ResourceSeederFileFactory::handle($resource);
+    }
+
+    /** @test */
+    public function it_calls_stub_builder_with_the_correct_params(): void
+    {
+        DirectoryMaker::shouldReceive('findOrMake')->once();
+        FileMaker::shouldReceive('make')->once();
+
+        $resource = new Resource($this->faker->word);
+
+        StubBuilder::shouldReceive('build')
+            ->with('seeder', $resource->toArray())
+            ->once();
+
+        ResourceSeederFileFactory::handle($resource);
+    }
+
+    /** @test */
+    public function it_calls_file_maker_with_the_correct_params(): void
+    {
+        DirectoryMaker::shouldReceive('findOrMake')->once();
+
+        $resource = new Resource($this->faker->word);
+        $content = $this->faker->paragraph;
+
+        StubBuilder::shouldReceive('build')->andReturn($content)->once();
+
+        FileMaker::shouldReceive('make')
+            ->with(database_path("seeds/{$resource->singularPascal()}TableSeeder.php"), $content)
+            ->once();
+
+        ResourceSeederFileFactory::handle($resource);
+    }
+}
