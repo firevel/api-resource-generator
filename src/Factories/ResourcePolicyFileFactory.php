a48715a304deac6365770e03fc011aa97a0baade
Gustavo Gentil <gustavorgentil@outlook.com>
2020-09-10 19:53:30 -0300

    docs: describe ResourcePolicyFileFactory

--- src/Factories/ResourcePolicyFileFactory.php
+++ src/Factories/ResourcePolicyFileFactory.php
@@ -4,18 +4,38 @@ namespace Firevel\ApiResourceGenerator\Factories;
 
 use Firevel\ApiResourceGenerator\Resource;
 
+/**
+ * Class ResourcePolicyFileFactory
+ * @package Firevel\ApiResourceGenerator\Factories
+ */
 class ResourcePolicyFileFactory extends StubbedResourceFileFactory
 {
+    /**
+     * Get resource file type.
+     *
+     * @return string
+     */
     protected static function getFileType(): string
     {
         return 'policy';
     }
 
+    /**
+     * Get resource file name.
+     *
+     * @param Resource $resource
+     * @return string
+     */
     protected static function getFileName(Resource $resource): string
     {
         return "{$resource->singularPascal()}Policy.php";
     }
 
+    /**
+     * Get resource file directory.
+     *
+     * @return string
+     */
     protected static function getFileDirectory(): string
     {
         return app_path('Policies');
