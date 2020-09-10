61cd0e6f71b5ebb2d9d2cd28caaafef9fb314e6e
Gustavo Gentil <gustavorgentil@outlook.com>
2020-09-10 14:54:46 -0300

    test: ensure it logs the command operations

--- tests/MakeApiResourceTest.php
+++ tests/MakeApiResourceTest.php
@@ -2,7 +2,36 @@
 
 namespace Firevel\ApiResourceGenerator\Tests;
 
+use Firevel\ApiResourceGenerator\DirectoryMakerFacade as DirectoryMaker;
+use Firevel\ApiResourceGenerator\Factories\ResourceControllerFileFactory;
+use Firevel\ApiResourceGenerator\Factories\ResourceModelFileFactory;
+use Firevel\ApiResourceGenerator\FileMakerFacade as FileMaker;
+use Firevel\ApiResourceGenerator\Resource;
+use Firevel\ApiResourceGenerator\StubBuilderFacade as StubBuilder;
+use Illuminate\Foundation\Testing\WithFaker;
+
 class MakeApiResourceTest extends TestCase
 {
+    use WithFaker;
 
+    /** @test */
+    public function it_logs_all_operations_correctly()
+    {
+        StubBuilder::shouldReceive('build');
+        DirectoryMaker::shouldReceive('findOrMake');
+        FileMaker::shouldReceive('make');
+
+        config(['api-resource-generator.types' => [
+            'Model',
+            'Controller',
+        ]]);
+
+        $resource = new Resource($this->faker->word);
+
+        $this->artisan("make:api-resource {$resource->raw()}")
+            ->expectsOutput("{$resource->singularPascal()} Model created.")
+            ->expectsOutput("{$resource->singularPascal()} Controller created.")
+            ->expectsOutput('Done.')
+            ->assertExitCode(0);
+    }
 }
