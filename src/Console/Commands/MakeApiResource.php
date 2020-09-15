cdae1b62caaec77f4b03b49edefbc9d5f38d9b72
Michael Slowik <slowik.michael@gmail.com>
2020-09-15 11:13:59 -0500

    Updated steps

--- src/Console/Commands/MakeApiResource.php
+++ src/Console/Commands/MakeApiResource.php
@@ -50,18 +50,18 @@ class MakeApiResource extends Command
 
         $this->info("\n");
         $this->info('To do:');
-        $this->info('1. Setup schema in migration file.');
-        $this->info('2. Setup \$fillables in model file.');
-        $this->info('3. Setup permissions in policy file.');
-        $this->info('4. Setup the transformer.');
-        $this->info("6. Register the policy: {$resource->pluralPascal()}::class => {$resource->pluralPascal()}Policy::class,.");
-        $this->info("5. Register the API route: Route::apiResource('{$resource->pluralSnake()}', '{$resource->pluralPascal()}Controller');.");
+        $this->info("1. Setup migration database/migrations/create_{$resource->pluralSnake()}_table.php");
+        $this->info("2. Setup \$fillables in app/Models/{$resource->singularPascal()}.php");
+        $this->info('3. Setup the transformer at app/Transformers/{$resource->singularPascal()}Transformer.php');
+        $this->info("4. Setup permissions in app/Policies/{$resource->pluralPascal()}Policy::class");
+        $this->info("5. Register the policy: {$resource->pluralPascal()}::class => {$resource->pluralPascal()}Policy::class.");
+        $this->info("6. Register the API route: Route::apiResource('{$resource->pluralSnake()}', '{$resource->pluralPascal()}Controller');.");
         $this->info("\n");
         $this->info('Optional:');
-        $this->info('- Setup model factory.');
-        $this->info('- Setup model seeder.');
-        $this->info('- Setup validation at requests.');
-        $this->info('- Setup unit tests for your model.');
-        $this->info('- Seed the database.');
+        $this->info("- Setup factory at database/factories/{$resource->singularPascal()}Factory.php");
+        $this->info("- Setup seeder at database/seeds/{$resource->pluralPascal()}Seeder.php");
+        $this->info("- Add {$resource->pluralPascal()}Seeder.php to database/seeds/DatabaseSeeder.php.");
+        $this->info("- Setup validation at Http/Requests/{$resource->singularPascal()}");
+        $this->info("- Setup unit tests at tests/Feature/{$resource->singularPascal()}");
     }
 }
