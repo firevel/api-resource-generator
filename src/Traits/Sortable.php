5c48696dc6899bdcb9ad0f8a0892359e4f9fcbc7
Michael Slowik <slowik.michael@gmail.com>
2020-10-02 11:44:19 -0500

    Allow builders other than Eloquent

--- src/Traits/Sortable.php
+++ src/Traits/Sortable.php
@@ -2,7 +2,6 @@
 
 namespace Firevel\ApiResourceGenerator\Traits;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Str;
 
 /**
@@ -18,7 +17,7 @@ trait Sortable
      * @param array $sortingAttributes
      * @return Builder
      */
-    public function scopeSort(Builder $query, array $sortingAttributes): Builder
+    public function scopeSort($query, array $sortingAttributes)
     {
         foreach ($sortingAttributes as $attribute) {
             $sortingDirection = strpos($attribute, '-') === 0 ? 'desc' : 'asc';
