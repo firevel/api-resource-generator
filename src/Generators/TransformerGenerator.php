5e4f7595d61d6ad3b2dc154c75b15c80c5db1217
Michael Slowik <slowik.michael@gmail.com>
2023-05-28 22:31:11 +0200

    use file generator for transformer

--- src/Generators/TransformerGenerator.php
+++ src/Generators/TransformerGenerator.php
@@ -9,17 +9,17 @@ class TransformerGenerator extends BaseGenerator
     public function generate()
     {
         $resource = $this->resource();
-        $name = $resource->name() . 'Transformer';
+        $name = $resource->singularPascal() . 'Transformer';
 
-        $this->artisan(
-            'make:transformer',
-            [
-                'name' => $name,
-            ]
-        );
+        $stub = $this->getStub('transformer');
+        $source = $this->buildStub($stub, $resource->toArray());
+        $path = app_path('Transformers') . '/' . "{$name}.php";
+
+        $this->createFile($path, $source);
+
         $this->logger()->info("# Transformer created: {$name}");
         $this->logger()->info('- [Optional] Set transformer fields');
-        $this->logger()->info('- [Optional] Set $availableIncludes fields');
+        $this->logger()->info('- [Optional] Set $availableIncludes fields and relationships');
         $this->logger()->info('  - Documentation https://fractal.thephpleague.com/transformers/#including-data');
     }
 }
