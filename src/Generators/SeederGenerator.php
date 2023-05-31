f8916044db0b9dc2b4e2e3c775244dcc905c95a6
Michael Slowik <slowik.michael@gmail.com>
2023-05-31 17:36:24 +0200

    updated seeder documentation

--- src/Generators/SeederGenerator.php
+++ src/Generators/SeederGenerator.php
@@ -19,6 +19,9 @@ class SeederGenerator extends BaseGenerator
         );
         $this->logger()->info("# Seeder created: {$name}");
         $this->logger()->info("- [Optional] Add factory to seeder");
-        $this->logger()->info("  - Example: {$name}::factory()->count(50)->create()");
+        $this->logger()->info("  - Example: \\App\Models\\" . $resource->name() . "::factory()->count(50)->create();");
+        $this->logger()->info("- [Optional] Add seeder to DatabaseSeeder");
+        $this->logger()->info("  - Add `$this->call({$name}::class);` to database/seeders/DatabaseSeeder.php");
+
     }
 }
