eb910fca9b977f0d2f6b3e04c7512877a761541e
Michael Slowik <slowik.michael@gmail.com>
2023-05-31 17:28:46 +0200

    route case fix

--- src/Generators/RouteGenerator.php
+++ src/Generators/RouteGenerator.php
@@ -9,8 +9,9 @@ class RouteGenerator extends BaseGenerator
     public function generate()
     {
         $resource = $this->resource();
+        $name = str_replace('_', '-', $resource->pluralSnake());
 
         $this->logger()->info("# Generating route");
-        $this->logger()->info("- [Required] Register the API route: Route::apiResource('{$resource->pluralSnake()}', Api\\{$resource->pluralPascal()}Controller::class);");
+        $this->logger()->info("- [Required] Register the API route: Route::apiResource('{$name}', \\App\\Http\\Controllers\\Api\\{$resource->pluralPascal()}Controller::class);");
     }
 }
